4def8c2da32b713269d04aa377657f6cce0bbd18
Frederick Son <fredson@redhat.com>
2025-03-03 10:49:28 +1100

    Add variable

--- terraform/terraform-aws/variables.tf
+++ terraform/terraform-aws/variables.tf
@@ -3,3 +3,33 @@ variable "region" {
  description = "Region Name"
  default     = "us-east-2"
 }
+
+variable "vpc_cidr" {
+ type        = string
+ description = "VPC cidr"
+ default     = "11.0.0.0/16"
+}
+
+variable "subnet_private_cidr" {
+ type        = string
+ description = "Subnet cidr"
+ default     = "11.0.2.0/24"
+}
+
+variable "subnet_public_cidr" {
+ type        = string
+ description = "Subnet cidr"
+ default     = "11.0.1.0/24"
+}
+
+variable "availability_zone" {
+ type        = string
+ description = "Subnet cidr"
+ default     = "us-east-2a"
+}
+
+variable "vm_name" {
+ type        = string
+ description = "Virtual Machine Name"
+ default     = "rhel9app.fredson.dev"
+}
