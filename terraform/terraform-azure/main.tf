54396b72a33b3a9b3428722614c16d21516c3c28
Frederick Son <fredson@redhat.com>
2025-03-03 16:39:26 +1100

    variable remove

--- terraform/terraform-azure/main.tf
+++ terraform/terraform-azure/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "azurerm" {
       resource_group_name  = "tfstate"
-      storage_account_name = "fredsostorage"
+      storage_account_name = "fredsonstorage"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
   }
