e7d8b981d1b9a18b8d70f628080696b5cc8768a1
Frederick Son <fredson@redhat.com>
2025-03-03 21:24:22 +1100

    Open WINRM port

--- terraform/terraform-azure/main.tf
+++ terraform/terraform-azure/main.tf
@@ -99,6 +99,18 @@ resource "azurerm_network_security_group" "tfnsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "WINRM"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5986"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "tfni" {
