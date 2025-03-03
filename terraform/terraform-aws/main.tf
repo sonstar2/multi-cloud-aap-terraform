17b9da0b10882ebbd0ba22278ff002f4a80d9b07
Frederick Son <fredson@redhat.com>
2025-03-04 10:07:47 +1100

    Remove aws_eip

--- terraform/terraform-aws/main.tf
+++ terraform/terraform-aws/main.tf
@@ -73,13 +73,13 @@ resource "aws_network_interface" "ansible-nic" {
     "Name" = "Ansible-Terraform-NI"
   }
 }
-resource "aws_eip" "ip-one" {
-  domain                    = "vpc"
-  network_interface         = aws_network_interface.ansible-nic.id
-  depends_on                = [aws_instance.app-server]
-  tags = {
-    "Name" = "Ansible-Terraform-EIP"
-  }
+# resource "aws_eip" "ip-one" {
+#   domain                    = "vpc"
+#   network_interface         = aws_network_interface.ansible-nic.id
+#   depends_on                = [aws_instance.app-server]
+#   tags = {
+#     "Name" = "Ansible-Terraform-EIP"
+#   }
 }
 resource "aws_security_group" "web-pub-sg" {
   name        = "Ansible_SG"                ### Survey
