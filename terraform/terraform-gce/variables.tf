9981b90b8d711dc7340ef09c1875b4dc2b333211
Frederick Son <fredson@redhat.com>
2025-03-04 20:30:29 +1100

    local backend

--- terraform/terraform-gce/variables.tf
+++ terraform/terraform-gce/variables.tf
@@ -1,8 +1,7 @@
 variable "gcp_project" {
  type        = string
  description = "Project Name"
- default     = "openenv-vczht
-"
+ default     = "openenv-vczht"
 }
 
 variable "region" {
@@ -10,46 +9,23 @@ variable "region" {
  description = "Region Name"
  default     = "asia-northeast3"
 }
+
 variable "zone" {
  type        = string
  description = "Zone Name"
  default     = "asia-northeast3-a"
 }
 
-variable "zone" {
+variable "machine_type" {
  type        = string
  description = "Machine Type"
- default     = "e2-mocro"
+ default     = "e2-micro"
 }
 
 variable "image" {
  type        = string
  description = "OS Image"
  default     = "debian-11-bullseye-v20240110"
-}
-
-variable "vpc_cidr" {
- type        = string
- description = "VPC cidr"
- default     = "11.0.0.0/16"
-}
-
-variable "subnet_private_cidr" {
- type        = string
- description = "Subnet cidr"
- default     = "11.0.2.0/24"
-}
-
-variable "subnet_public_cidr" {
- type        = string
- description = "Subnet cidr"
- default     = "11.0.1.0/24"
-}
-
-variable "availability_zone" {
- type        = string
- description = "Subnet cidr"
- default     = "us-east-2a"
 }
 
 variable "vm_name" {
