54bd97e40375b7a65dfc4d78a7257ca7a583d07d
Frederick Son <fredson@redhat.com>
2025-03-05 19:54:51 +1100

    gcp output

--- terraform/terraform-gce/output.tf
+++ terraform/terraform-gce/output.tf
@@ -1,7 +1,7 @@
-output "tf-demo-gcp-instance-1" {
+output "host_name" {
   value = google_compute_instance.tf-demo-gcp-instance-1.tags.Name
 }
 
-output "tf-demo-gcp-instance-1" {
+output "public_ip" {
   value = network_interface.access_config.nat_ip
 }
