3de4534b132ccd85276e83f48a97e5ebe836d27f
Frederick Son <fredson@redhat.com>
2025-03-10 14:34:42 +1100

    Use app-server

--- terraform/terraform-gce/main.tf
+++ terraform/terraform-gce/main.tf
@@ -57,7 +57,8 @@ resource "google_compute_firewall" "firewall" {
 #   network_tier = "PREMIUM"
 # }
 
-resource "google_compute_instance" "tf-demo-gcp-instance-1" {
+resource "google_compute_instance" "app-server" {
+# resource "google_compute_instance" "tf-demo-gcp-instance-1" {
   name         = var.vm_name
   machine_type = var.machine_type
   zone = var.zone
